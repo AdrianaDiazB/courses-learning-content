f2813a76a5e080d1fe6d2b4bee590ba4ae29de1a
Johan-Romo <jaromo3@espe.edu.ec>
2023-11-14 15:44:33 -0500

    Update Detalles

--- front/src/home/components/preguntas/detalles/Detalles.js
+++ front/src/home/components/preguntas/detalles/Detalles.js
@@ -1,41 +1,55 @@
+
 import React from "react";
 
 import raya from "../imgs/raya.png";
 
 export const Detalles = () => {
   return (
-     <div className="container">
-   <h2 className="display-5">Contactos</h2>
+    <div className="container mt-5">
+      <h2 className="display-5">Contactos</h2>
       <img className="image" src={raya} alt="" width="500" height="50" />
 
-      <h5 className="display-100">RESPONSABLE</h5>
-      Ing. Luis Enrique Simbaña Ph.D
-      <br />
-      <br />
+      <div className="contacto">
+        <h5 className="display-100">RESPONSABLE</h5>
+        <p>Ing. Luis Enrique Simbaña Ph.D</p>
+      </div>
 
-      <h5 className="display-100">CORREO</h5>
-      lesimbania@espe.edu.ec
-      <br />
-      <br />
+      <div className="contacto">
+        <h5 className="display-100">CORREO</h5>
+        <p>lesimbania@espe.edu.ec</p>
+      </div>
 
-      <h5 className="display-100">TELEFONO</h5>
-      (02) 356 4427
-      <br />
-      <br />
+      <div className="contacto">
+        <h5 className="display-100">TELÉFONO</h5>
+        <p>(02) 356 4427</p>
+      </div>
 
-      <h5 className="display-100">CELULAR</h5>
-      09981602933
-      <br />
-      <br />      
+      <div className="contacto">
+        <h5 className="display-100">CELULAR</h5>
+        <p>09981602933</p>
+      </div>
 
-      <h5 className="display-100">WHATSAPP</h5>
-      (593) 98 160 2933
-      <br />
-      <br />
+      <div className="contacto">
+        <h5 className="display-100">WHATSAPP</h5>
+        <p>(593) 98 160 2933</p>
+      </div>
 
-    
-      
+      <div className="contacto">
+        <h5 className="display-100">NUEVO CONTACTO</h5>
+        <p>Lcda. Lena Ivannova Ruiz Rojas, PHD</p>
+      </div>
 
+      <div className="contacto">
+        <h5 className="display-100">CORREO</h5>
+        <p>liruiz@espe.edu.ec</p>
+      </div>
+
+      <div className="contacto">
+        <h5 className="display-100">TELÉFONO</h5>
+        <p>+593 98 168 4393</p>
+      </div>
     </div>
   );
 };
+
+export default Detalles;
