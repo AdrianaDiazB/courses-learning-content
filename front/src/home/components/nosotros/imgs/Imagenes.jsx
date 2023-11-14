53f346c8213fd477ec10f3f2ca263fc694789f24
HeidyBenalcazar <135286950+HeidyBenalcazar@users.noreply.github.com>
2023-11-14 14:49:12 -0500

    Update images nosotros

---
+++ front/src/home/components/nosotros/imgs/Imagenes.jsx
@@ -0,0 +1,13 @@
+import gestion from '../img/Gestion.jpg'
+import estrategico from '../img/Estrategico.jpg'
+import mision from '../img/Mision.jpg'
+import vision from '../img/Vision.jpg'
+// import prowess_mocc from '../../img/prowess_mooc.webp' --> Manera de poner una segunda imagen
+
+export default{
+    "gestion": gestion,
+    // "img1": prowess_mocc --> Manera de poner una segunda imagen
+    "estrategico": estrategico,
+    "mision": mision,
+    "vision": vision
+}
