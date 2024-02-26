e6deaad1a8d583ca47ae5e8b4e53776d6344626a
Adriana Lizbeth Diaz Bautista <aldiaz5@espe.edu.ec>
2024-02-26 18:51:35 -0500

    Update cursos page

--- front/src/home/pages/CursosPage.jsx
+++ front/src/home/pages/CursosPage.jsx
@@ -5,6 +5,8 @@ import Footer from "../components/footer/Footer";
 import { useParams } from "react-router-dom";
 import { getCourses } from "../../services/backend-api";
 import Loading from "../components/loader/Loading";
+import CursoGeneral from "../components/inicio/CursoGeneral";
+import Linea from "../components/inicio/Linea";
 
 const CursosPage = () => {
   const { tipos } = useParams();
@@ -44,10 +46,49 @@ const CursosPage = () => {
           Modulos completamente gratuitos en modalidad virtual para estudiantes
         </p>
         <ListCurso tipos={tipos} courses={courses} />
+        <div className="container">
+        <Linea />
+        <div class="row gx-0 d-flex justify-content-center gx-3 mb-5 text-black">
+          <CursoGeneral
+            title={"MATEMÁTICAS"}
+            img={
+              "https://res.cloudinary.com/dzcke9fzx/image/upload/v1702517752/ANALISIS/mate_cw0nfp.jpg"
+            }
+           
+            
+          />
+
+          <CursoGeneral
+            title={"CIENCIAS NATURALES"}
+            img={
+              "https://res.cloudinary.com/dzcke9fzx/image/upload/v1702516908/ANALISIS/quimica_hlh8yj.jpg"
+            }
+            
+          />
+
+          <CursoGeneral
+            title={"LENGUA Y LITERATURA"}
+            img={
+              "https://res.cloudinary.com/dzcke9fzx/image/upload/v1702516906/ANALISIS/lenjuaje_c0avqq.jpg"
+            }
+           
+          />
+
+          <CursoGeneral
+            title={"ESTUDIOS SOCIALES"}
+            img={
+              "https://res.cloudinary.com/dzcke9fzx/image/upload/v1702516904/ANALISIS/sociales_ltmzwp.jpg"
+            }
+            
+          />
+        </div>
+      </div>
       </div>
       <Footer />
     </>
   );
+
+
 };
 
 export default CursosPage;
